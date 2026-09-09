0ad5f133b28862a9de60471d67f2b2bef89be98d
fabian.frei <fabian.frei1@students.fhnw.ch>
2026-09-09 15:34:51 +0200

    feat(lsp): enable additional language servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,50 @@
 require("mason").setup()
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"clangd",
+		"cssls",
+		"gopls",
+		"groovyls",
+		"hls",
+		"html",
+		"intelephense",
+		"jdtls",
+		"jsonls",
+		"lua_ls",
+		"omnisharp",
+		"ts_ls",
+		"pyright",
+		"ruby_lsp",
+		"terraformls",
+		"yamlls",
+    "rust_analyzer",
+	},
+	automatic_enable = false,
+})
+
+vim.lsp.enable({
+	"clangd",
+	"cssls",
+	"gopls",
+	"groovyls",
+	"hls",
+	"html",
+	"intelephense",
+	"jdtls",
+	"jsonls",
+	"lua_ls",
+	"omnisharp",
+	"ts_ls",
+	"pyright",
+	"ruby_lsp",
+	"terraformls",
+	"yamlls",
+  "rust_analyzer",
+})
+
+if vim.fn.exists(":LspInfo") == 0 then
+	vim.api.nvim_create_user_command("LspInfo", "checkhealth vim.lsp", {})
+end
  
 -- vim.lsp.config("ts_ls", {
 -- 	-- Server-specific settings. See `:help lsp-quickstart`
@@ -15,4 +60,3 @@ vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
 vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
